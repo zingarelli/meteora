d00c355b657de7230e7e747ee63452905d762732
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-06-26 14:16:48 -0300

    add font using next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './reset.css';
-import './global.css';
-import StyledComponentsRegistry from "@/lib/registry"
+import './globals.css';
+import StyledComponentsRegistry from "@/lib/registry";
+import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 
 export const metadata = {
@@ -8,13 +9,19 @@ export const metadata = {
   description: 'Projeto criado para o Challenge Front End da Alura de junho/2023. Criação da página de e-commerce para a Meteora. A Meteora é uma empresa do comércio eletrônico de roupas atemporais, casuais e sem gênero.',
 }
 
+// Inter font with Latin characters
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={inter.className}>
       <body>
         <StyledComponentsRegistry>
           <Header />
